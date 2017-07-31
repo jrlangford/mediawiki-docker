84e0b13e14cc02ffac1cc743f3f6c72bb062e37c
Jonathan Robin Langford <jrobinlangford@gmail.com>
2017-07-30 22:14:39 -0500

    Fix parsoid link script

--- link-parsoid.sh
+++ link-parsoid.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 WIKI_IP=`docker inspect \
 	-f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' \
-	mediawikidocker_wiki_1`
+	mediawikidocker_web_1`
 docker exec mediawikidocker_parsoid_1 bash -c "echo \"$WIKI_IP wiki\" >> /etc/hosts"
